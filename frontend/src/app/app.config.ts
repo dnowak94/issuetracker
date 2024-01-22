6003a208bafb69d25af81d493d87a670abfa5cdd
Dominik Nowak <dnowak94@live.de>
2024-01-22 21:28:10 +0100

    initial frontend commit with issues list and navigation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,15 +5,10 @@ import localeDeExtra from '@angular/common/locales/extra/de';
 import { registerLocaleData } from '@angular/common';
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-import { provideNgIconsConfig } from '@ng-icons/core';
 
 
 registerLocaleData(localeDe, localeDeExtra);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom(HttpClientModule), {provide: LOCALE_ID, useValue:'de'},
-    provideNgIconsConfig({
-      size: '1.5em'
-    })
-  ]
+  providers: [provideRouter(routes),importProvidersFrom(HttpClientModule), {provide: LOCALE_ID, useValue:'de'}]
 };
