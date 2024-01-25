394df33bbb54308d3537347b2569b92790e842e4
Dominik Nowak <dnowak94@live.de>
2024-01-25 20:34:28 +0100

    implement Read and Update functionality in issue service

--- frontend/src/app/services/issues.service.ts
+++ frontend/src/app/services/issues.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Issue } from '../model/Issue';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class IssuesService {  
-  
-  constructor(private http:HttpClient) {}
-  
-  getIssues():Observable<Issue[]> {
-    console.log(`url: ${environment.ISSUES_URL}`);
+export class IssuesService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getIssues(): Observable<Issue[]> {
     return this.http.get<Issue[]>(environment.ISSUES_URL)
       .pipe(
         tap(_ => console.log("fetched issues"))
+      );
+  }
+  getIssue(id: number): Observable<Issue> {
+    return this.http.get<Issue>(`${environment.ISSUES_URL}/${id}`)
+      .pipe(
+        tap(_ => console.log("fetched issue with id=", id))
+      );
+  }
+  updateIssue(issue: Issue): Observable<any> {
+    return this.http.put(`${environment.ISSUES_URL}/${issue.id}`, issue, this.httpOptions)
+      .pipe(
+        tap(_ => console.log("save issue:", JSON.stringify(issue)))
       );
   }
 }
