c68981e91f3defd15ca6bd9f281ecdf066e4a5f7
Dominik Nowak <dnowak94@live.de>
2024-04-22 12:50:38 +0200

    fix CORS in backend

--- backend/issue_service/src/main/java/com/example/issue_service/IssuetrackerApplication.java
+++ backend/issue_service/src/main/java/com/example/issue_service/IssuetrackerApplication.java
@@ -17,7 +17,7 @@ public class IssuetrackerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/issues")
+				registry.addMapping("/issues/**")
 				.allowedOrigins("http://localhost:4200")
 				.allowedMethods("GET", "HEAD", "OPTIONS", "PUT", "POST", "DELETE");
 			}
