0b1bb2ee5ef2f069a64ada2a5a80534d252e38ef
Dominik Nowak <dnowak94@live.de>
2024-06-11 13:26:10 +0200

    add dev and prod profiles in backend

--- backend/projects/src/main/resources/data.sql
+++ backend/projects/src/main/resources/data.sql
@@ -1,6 +1,9 @@
-INSERT INTO PROJECT (id, name)
-VALUES (1, 'Issuetracker');
+INSERT INTO project (id, name, description) VALUES
+(1, 'Issuetracker', '<p>This project is an application for tracking issues and tasks of a project.</p>');
 
-INSERT INTO TASK (id, title, description, status, created_at, updated_at, project_id)
-VALUES (1,'Implement JWT Authentication', '','TODO', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 1),
-(2, 'Add Spring Security Configuration', '','TODO',CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 1);
+INSERT INTO task (id, title, description, status, created_at, updated_at, project_id) VALUES 
+(1,'Implement JWT Authentication', '','TODO', NOW(), NOW(), 1),
+(2, 'Add Spring Security Configuration', '','TODO',NOW(), NOW(), 1);
+
+INSERT INTO issue (id, title, description, status, created_at, updated_at, project_id) VALUES
+(1,'Edit form', '<p>edit form does not initilialize values from backend</p>','UNRESOLVED', NOW(), NOW(), 1);
